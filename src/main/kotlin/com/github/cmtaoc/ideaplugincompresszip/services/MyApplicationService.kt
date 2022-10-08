58958584620409ee4d6c65020b4a935e51be03aa
GitHub Action <action@github.com>
2022-10-08 12:54:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/cmtaoc/ideaplugincompresszip/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/cmtaoc/ideaplugincompresszip/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.cmtaoc.ideaplugincompresszip.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cmtaoc.ideaplugincompresszip.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/cmtaoc/ideaplugincompresszip/services/MyProjectService.kt
+++ src/main/kotlin/com/github/cmtaoc/ideaplugincompresszip/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.cmtaoc.ideaplugincompresszip.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cmtaoc.ideaplugincompresszip.MyBundle
 
 class MyProjectService(project: Project) {
 
